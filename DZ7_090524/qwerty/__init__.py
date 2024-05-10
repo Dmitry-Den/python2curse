c4fed3c814a78c78bdc3870d378f2e63803cf622
dimka <e.atagazina@pravo.tech>
2024-05-10 16:10:47 +0400

    initial commit

--- DZ7_090524/qwerty/__init__.py
+++ DZ7_090524/qwerty/__init__.py
@@ -1,2 +1,9 @@
 from qwerty.generate_files import generate_files
 from qwerty.rename_files import group_rename_files
+
+
+from qwerty import generate_files, group_rename_files
+
+# Использование функций из пакета
+generate_files('test', 'files', 5)
+group_rename_files('new_file_', 3, '.txt', '.docx', name_range=(3, 6))
