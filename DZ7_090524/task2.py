0498636f66c6a8a96c83e2432fe815ac9428f1a2
dimka <e.atagazina@pravo.tech>
2024-05-10 15:24:19 +0400

    initial commit

--- DZ7_090524/task2.py
+++ DZ7_090524/task2.py
@@ -12,19 +12,19 @@
 
 import os
 
-def group_rename_files(desired_name, num_digits, original_extension, final_extension, name_range=None):
+def group_rename_files(target_name, digits_count, original_extension, new_extension, name_range=None):
     files = [f for f in os.listdir() if f.endswith(original_extension)]
 
-    count = 1
-    for file in files:
-        if name_range:
-            original_name = file[name_range[0]-1:name_range[1]] + desired_name
-        else:
-            original_name = desired_name
+    if name_range:
+        start, end = name_range
+    else:
+        start, end = 0, None
 
-        new_name = f"{original_name}_{str(count).zfill(num_digits)}.{final_extension}"
-        os.rename(file, new_name)
-        count += 1
+    counter = 1
+    for file_name in files[start:end]:
+        new_file_name = target_name + str(counter).zfill(digits_count) + new_extension
+        os.rename(file_name, new_file_name)
+        counter += 1
 
 # Пример использования функции
-group_rename_files("234", 3, ".txt", "txt", name_range=(3, 6))
+group_rename_files("new_file", 3, ".txt", ".docx", name_range=(1, 4))
